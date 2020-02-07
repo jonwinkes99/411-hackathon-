a033ed93cb3d80b9b99e53ebf5c9535fc28c4c93
winkleman <jwinks18@gmail.com>
2020-02-07 10:44:37 -0600

    first try

---
+++ 411-hackathon/src/Components/ComOne.js
@@ -0,0 +1,41 @@
+import react, { Component } from "react"
+
+class Alt extends Component {
+    state = {
+        alts: [],
+        input: ""
+      }
+
+
+      onChange = (e) => {
+        this.setState({input: e.target.value})
+    
+      }
+
+      fetchArticles = (e) => {
+        e.preventDefault()
+        fetch("https://hn.algolia.com/api/v1/search?query=" + this.state.input)
+          .then(res => res.json())
+          .then(response => {
+            console.log(response)
+            this.setState({ alts: response.hits})
+          });
+      };
+
+      render() {
+        return (
+            <form onSubmit={this.fetchArticles}>
+            <input onChange={this.onChange}></input>
+            <button type="submit">Submit</button>
+            {
+              this.state.alts.map(Alt => {
+                return <p> <a href={arc.url}>{arc.title}</a></p>
+              })
+            }
+          </form>
+        );
+    }
+
+}
+
+export default Alt
